4ae2abd5baf8a4b5db2c5643ef5a3e745ee61e11
Blanco <105171603+blancooooz@users.noreply.github.com>
2024-10-04 14:23:40 -0400

    Update DataHandler.js

--- src/utils/DataHandler.js
+++ src/utils/DataHandler.js
@@ -16,13 +16,7 @@ import { initializeFirestore } from "firebase/firestore";
 
 // Firebase Import
 const firebaseConfig = {
-  apiKey: "AIzaSyCq4BF6OciC4FVEZ_F10QmrmoLGkWeFXqE",
-  authDomain: "study-app-bc788.firebaseapp.com",
-  projectId: "study-app-bc788",
-  storageBucket: "study-app-bc788.appspot.com",
-  messagingSenderId: "214203925511",
-  appId: "1:214203925511:web:5b3bfc05b4ede20e99d6a2",
-  measurementId: "G-ZLQ79S8Y48"
+  //OUR FIREBASE CONFIG
 };
 // Initialize app and auth
 let app, firebaseAuth;
