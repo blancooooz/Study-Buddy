273970a0abe0f7e5818961f8c749e14d8371e47e
Blanco <105171603+blancooooz@users.noreply.github.com>
2024-10-16 15:23:56 -0400

    Update Daily.js

--- src/screens/daily/Daily.js
+++ src/screens/daily/Daily.js
@@ -1,17 +1,22 @@
 import { Button, View } from "react-native";
 
-const Daily = ({navigation}) => {
-  return <View>
-
-    <Button
-    title="Prefs"
-    onPress={()=>{navigation.navigate("Preferences")}}>
-
-    </Button>
-    <Button
-    title="Settings"
-    onPress={()=>{navigation.navigate("Settings")}}></Button>
-  </View>;
+const Daily = ({ navigation }) => {
+  return (
+    <View>
+      <Button
+        title="Prefs"
+        onPress={() => {
+          navigation.navigate("Preferences");
+        }}
+      ></Button>
+      <Button
+        title="Settings"
+        onPress={() => {
+          navigation.navigate("Settings");
+        }}
+      ></Button>
+    </View>
+  );
 };
 
 export default Daily;
