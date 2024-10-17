53055ae00bf9c34fe4416846aba2c329e37af77c
lauraforero12 <lauraforerogarzon@gmail.com>
2024-10-17 14:29:23 -0400

    Update Settings.js
    m

--- src/screens/account/Settings.js
+++ src/screens/account/Settings.js
@@ -10,7 +10,7 @@ const Settings = ({ navigation }) => {
         }}
       ></Button>
       <Button
-        title="Username"
+        title="Change username"
         onPress={() => {
           navigation.navigate("Username");
         }}
