76d4468d6cfe43545d1a5c62d556188289cff15d
Blanco <105171603+blancooooz@users.noreply.github.com>
2024-11-11 22:09:59 -0500

    Revert "Just removed header and sessions add"
    This reverts commit 07bd1f2faece9c45fa563f300abb5313e1ba77dc.

--- src/screens/study/StudyPlans.js
+++ src/screens/study/StudyPlans.js
@@ -30,17 +30,10 @@ const StudyPlan = ({ navigation }) => {
   const progressCircle = () => {};
 
   const renderStudyPlan = ({ item }) => {
-    console.log(item)
-    let sessionsCompleted = 0;
-    try {
-      sessionsCompleted = item.sessions.filter(
-        (session) => session.completed
-      ).length;
-    } catch (e) {}
-    let totalSessions = 0;
-    try {
-      totalSessions = item.sessions.length;
-    } catch (e) {}
+    const sessionsCompleted = item.sessions.filter(
+      (session) => session.completed
+    ).length;
+    const totalSessions = item.sessions.length;
     const progress =
       totalSessions > 0 ? (sessionsCompleted / totalSessions) * 100 : 0;
 
